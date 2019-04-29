5f5f503ad398f6339164cd02c6565f4921225f34
Airstryk-1 <cj2027@yahoo.com>
2019-04-29 13:24:25 -0700

    Decode Function

--- drill2.js
+++ drill2.js
@@ -21,3 +21,33 @@ function beyond(num){
         console.log('Staying home');
     }
 }
+
+function decode(word){
+    let result = 0;
+    if (word[0] === 'a'){
+        result = word[1];
+    }
+
+    else if (word[0] === 'b'){
+        result = word[2];
+    }
+    else if (word[0] === 'c') {
+        result = word[3];
+    }
+    else if (word[0] === 'd') {
+        result = word[4];
+    }
+    else {
+        result = ' ';
+    }
+
+return result;
+}
+console.log(decode('craft'));
+console.log(decode('block'));
+console.log(decode('argon'));
+console.log(decode('meter'));
+console.log(decode('bells'));
+console.log(decode('brown'));
+console.log(decode('croon'));
+console.log(decode('droop'));
