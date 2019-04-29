3d07751e5cd3f60ab8ac8ba410390fd03eb8bdaa
Jonathan Moore <jmjonemoore@gmail.com>
2019-04-29 13:50:32 -0500

    Try Catch error

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+"use strict";
+
 function createGreeting(name, age){
+    if(age < 0){
+        throw new Error("Age can not be negative");
+    } 
  return `Hi, my name is ${name} and I'm ${age} years old.`;
 }
 console.log(createGreeting());
 
 function getYearOfBirth(age){
+    if(age < 0){
+        throw new Error("Age can not be negative")
+    }
     const yearOfBirth = 2019 - age;
     return `I was born in ${yearOfBirth}`;
+}
+
+try{
+    const greeting1 = createGreeting('Jonathan',-1);
+} catch(e){
+    console.error(e);
 }
