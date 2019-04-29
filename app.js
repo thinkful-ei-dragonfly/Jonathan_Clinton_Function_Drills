7eceb6c41ac7ba3876170ac9e128e07b950140b6
Jonathan Moore <jmjonemoore@gmail.com>
2019-04-29 13:41:54 -0500

    Add Year of Birth function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 function createGreeting(name, age){
-const yearOfBirth = 2019 - age;
  return `Hi, my name is ${name} and I'm ${age} years old.`;
- return `I was born in ${yearOfBirth}`;
 }
 console.log(createGreeting());
+
+function getYearOfBirth(age){
+    const yearOfBirth = 2019 - age;
+    return `I was born in ${yearOfBirth}`;
+}
