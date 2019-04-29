9ea8ae1a0d96a9c8b475052f37fd53723f0cea20
Jonathan Moore <jmjonemoore@gmail.com>
2019-04-29 13:58:48 -0500

    Different invalid parameters

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ function createGreeting(name, age){
     if((name === undefined)||(age === undefined)){
         throw new Error('Arugments not valid');
     }
+    if(typeof name !== "string"){
+        throw new Error('Name should be a string.');
+    }
+    if(typeof age !== "number"){
+        throw new Error('Age should be a number.');
+    }
  return `Hi, my name is ${name} and I'm ${age} years old.`;
 }
 console.log(createGreeting());
