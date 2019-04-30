8c92db4aa3b1f82bcd26186a2b3d704d06a08184
Jonathan Moore <jmjonemoore@gmail.com>
2019-04-30 12:41:24 -0500

    Using Strict Mode

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-"use strict";
+'use strict';
 function createGreeting(name, age){
-    if(age < 0){
-        throw new Error('Age can not be negative');
-    } 
-    if((name === undefined)||(age === undefined)){
-        throw new Error('Arugments not valid');
-    }
-    if(typeof name !== "string"){
-        throw new Error('Name should be a string.');
-    }
-    if(typeof age !== "number"){
-        throw new Error('Age should be a number.');
-    }
- return `Hi, my name is ${name} and I'm ${age} years old.`;
+  if(age < 0){
+    throw new Error('Age can not be negative');
+  } 
+  if((name === undefined)||(age === undefined)){
+    throw new Error('Arugments not valid');
+  }
+  if(typeof name !== "string"){
+    throw new Error('Name should be a string.');
+  }
+  if(typeof age !== "number"){
+    throw new Error('Age should be a number.');
+  }
+  return `Hi, my name is ${name} and I'm ${age} years old.`;
 }
 console.log(createGreeting());
 
 function getYearOfBirth(age){
-    if(age < 0){
-        throw new Error("Age can not be negative")
-    }
-    if((name === undefined)||(age === undefined)){
-        throw new Error('Arugments not valid');
-    }
-    const yearOfBirth = 2019 - age;
-    return `I was born in ${yearOfBirth}`;
+  if(age < 0){
+    throw new Error("Age can not be negative")
+  }
+  if((name === undefined)||(age === undefined)){
+    throw new Error('Arugments not valid');
+  }
+  const yearOfBirth = 2019 - age;
+  return `I was born in ${yearOfBirth}`;
 }
 
 try{
-    const greeting1 = createGreeting('Jonathan',-1);
+  const greeting1 = createGreeting('Jonathan',-1);
 } catch(e){
-    console.error(e);
+  console.error(e);
 }
--- drill2.js
+++ drill2.js
@@ -1,48 +1,48 @@
-"use strict";
+'use strict';
 function jediName(firstName, lastName){
-    let jedi = lastName.substring(0, 3).concat(firstName.substring(0, 2))
-    return jedi;
+  let jedi = lastName.substring(0, 3).concat(firstName.substring(0, 2))
+  return jedi;
 }
 console.log(jediName('Clinton', 'Carter'));
 
 function beyond(num){
-    if (num === infinity){
-        console.log('and beyond');
-    }
+  if (num === infinity){
+    console.log('and beyond');
+  }
     
-    else if (num > 0){
-        console.log('To infinity');
-    }
+  else if (num > 0){
+    console.log('To infinity');
+  }
     
-    else if(num < 0){
-        console.log('To negative infinity');
-    }
+  else if(num < 0){
+    console.log('To negative infinity');
+  }
 
-    else {
-        console.log('Staying home');
-    }
+  else {
+    console.log('Staying home');
+  }
 }
 
 function decode(word){
-    let result = 0;
-    if (word[0] === 'a'){
-        result = word[1];
-    }
+  let result = 0;
+  if (word[0] === 'a'){
+    result = word[1];
+  }
 
-    else if (word[0] === 'b'){
-        result = word[2];
-    }
-    else if (word[0] === 'c') {
-        result = word[3];
-    }
-    else if (word[0] === 'd') {
-        result = word[4];
-    }
-    else {
-        result = ' ';
-    }
+  else if (word[0] === 'b'){
+    result = word[2];
+  }
+  else if (word[0] === 'c') {
+    result = word[3];
+  }
+  else if (word[0] === 'd') {
+    result = word[4];
+  }
+  else {
+    result = ' ';
+  }
 
-return result;
+  return result;
 }
 console.log(decode('craft'));
 console.log(decode('block'));
@@ -54,77 +54,77 @@ console.log(decode('croon'));
 console.log(decode('droop'));
 
 function howManyDays(month){
-    let result = '';
-    switch(month){
-        case 'April':
-        case 'June':
-        case 'September':
-        case 'November':
-            result = `${month} has 30 days`;
-            break;
-    }
-    switch(month){
-        case 'January':
-        case 'March':
-        case 'May':
-        case 'July':
-        case 'August':
-        case 'October':
-        case 'December':
-            result = `${month} has 31 days`;
-            break;
-    }
-    switch(month){
-        case 'February':
-            result = `${month} has 28 days`;
-            break;
-    }
-    return result;
+  let result = '';
+  switch(month){
+  case 'April':
+  case 'June':
+  case 'September':
+  case 'November':
+    result = `${month} has 30 days`;
+    break;
+  }
+  switch(month){
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    result = `${month} has 31 days`;
+    break;
+  }
+  switch(month){
+  case 'February':
+    result = `${month} has 28 days`;
+    break;
+  }
+  return result;
 }
 console.log(howManyDays('March'));
 console.log(howManyDays('April'));
 console.log(howManyDays('February'));
 
 function RPS(num){
-    let result = '';
-    if((num > 3)||(num < 1)){
-        throw new error('Number should be between 1-3.');
+  let result = '';
+  if((num > 3)||(num < 1)){
+    throw new error('Number should be between 1-3.');
+  }
+  const randomNO = Math.floor(Math.random()*3) + 1;
+  if(num === 1){
+    if(randomNO === 2){
+      result = 'You lose';
     }
-    const randomNO = Math.floor(Math.random()*3) + 1;
-    if(num === 1){
-        if(randomNO === 2){
-            result = 'You lose';
-        }
-        else if(randomNO === 3){
-            result = 'You win';
-        }
-        else{
-            result = `It's a draw`;
-        }
+    else if(randomNO === 3){
+      result = 'You win';
     }
-    else if(num === 2){
-        if(randomNO > 2){
-            result = 'You lose'; 
-        }
-        else if(randomNO < 2){
-            result = 'You win';
-        }
-        else{
-            result = `It's a draw`;
-        }
+    else{
+      result = `It's a draw`;
     }
-    else if(num === 3){
-        if(randomNO === 1){
-            result = 'You lose';
-        }
-        else if(randomNO === 2){
-            result = 'You win';
-        }
-        else{
-            result = `It's a draw`;
-        }
+  }
+  else if(num === 2){
+    if(randomNO > 2){
+      result = 'You lose'; 
     }
-    return result;
+    else if(randomNO < 2){
+      result = 'You win';
+    }
+    else{
+      result = `It's a draw`;
+    }
+  }
+  else if(num === 3){
+    if(randomNO === 1){
+      result = 'You lose';
+    }
+    else if(randomNO === 2){
+      result = 'You win';
+    }
+    else{
+      result = `It's a draw`;
+    }
+  }
+  return result;
 }
 
 console.log(RPS(1));
